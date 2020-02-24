1b4340c31f3c43a343e3c981e3dcd7eded0423d5
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2020-02-24 21:48:28 +0100

    Fix mistake in comment.

--- signals.go
+++ signals.go
@@ -14,7 +14,7 @@ import "os"
 // SigAbort waits for a value on the ready channel.
 // Waiting is be aborted, when a value is received on the abort channel first.
 // It returns a channel that receives a value when either condition has been met,
-// true if ready was first to receive, true otherwise.
+// true if ready was first to receive, false otherwise.
 func SigAbort(ready chan bool, abort chan os.Signal) chan bool {
 	fin := make(chan bool)
 	go (func() {
